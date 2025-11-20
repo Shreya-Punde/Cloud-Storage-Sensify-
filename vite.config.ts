2dcd3769302dcdc551cd271f3fdfab9a4906e87b
S Giri Shankar <jrironman196@gmail.com>
2025-11-20 10:16:32 +0000

    some small changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig } from "vite";
 
-<<<<<<< HEAD
 export default defineConfig({
   // your config here
-=======
-export default defineConfig(({ mode }) => {
-    base: '/Cloud-Storage-Sensify-/',
-    plugins: [react()],
->>>>>>> 5782925fd679d8a302dcc1d943d080d4acabaa6f
 });
