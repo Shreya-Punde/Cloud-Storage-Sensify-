4adf9771056a256fdc1ee8cf3c0186cbb0f88336
S Giri Shankar <jrironman196@gmail.com>
2025-12-05 13:19:19 +0530

    Update image source path for goobe.PNG

--- App.tsx
+++ App.tsx
@@ -83,7 +83,7 @@ function App() {
                     d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
                   />
                 </svg> */}
-                <img className="z-100 h-10 w-10" src="./components/icons/goobe.PNG" alt="" />
+                <img className="z-100 h-10 w-10" src="./goobe.PNG" alt="" />
               </div>
 
               <div>
